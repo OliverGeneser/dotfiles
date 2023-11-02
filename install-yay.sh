5c867702469e855603c319de9de55c058c5a44d3
Oliver Geneser <32340667+OliverGeneser@users.noreply.github.com>
2023-11-02 19:52:31 +0000

    Create install-yay.sh

---
+++ install-yay.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+pacman -S --needed git base-devel
+git clone https://aur.archlinux.org/yay-bin
+cd yay-bin
+makepkg -si
+cd .. && rm -rf yay-bin
+
+yay -Y --gendb
+yay -Syu --devel
+yay -Y --devel --save
