c97fbba0a9680ecd4a24b3be23e69ac35928d1cf
Oliver Geneser <32340667+OliverGeneser@users.noreply.github.com>
2023-11-24 22:08:22 +0000

    Create install-nvidia.sh

---
+++ install-nvidia.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Tools
+yay -S hyprland-nvidia-git nvidia-dkms qt5-wayland qt5ct libva libva-nvidia-driver-git
+
+sudo systemctl enable nvidia-suspend.service
+sudo systemctl enable nvidia-hibernate.service
+sudo systemctl enable nvidia-resume.service
+
+# nvidia.NVreg_PreserveVideoMemoryAllocations=1
