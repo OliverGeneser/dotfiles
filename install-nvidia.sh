e9a2c5655a16eadc27453163385f665e60170c93
Oliver Geneser <32340667+OliverGeneser@users.noreply.github.com>
2023-11-25 15:06:23 +0100

    Update install-nvidia.sh

--- install-nvidia.sh
+++ install-nvidia.sh
@@ -2,7 +2,7 @@
 # https://wiki.hyprland.org/Nvidia/
 
 # Tools
-yay -S hyprland-nvidia-git nvidia-dkms qt5-wayland qt5ct libva libva-nvidia-driver-git
+yay -S hyprland-nvidia-git nvidia-dkms qt5-wayland qt5ct libva libva-nvidia-driver-git linux-headers
 
 sudo systemctl enable nvidia-suspend.service
 sudo systemctl enable nvidia-hibernate.service
