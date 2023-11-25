8601658b10b2899087364d24c1a4841039d3f389
OliverGeneser <32340667+OliverGeneser@users.noreply.github.com>
2023-11-25 18:23:38 +0100

    Added configure step

--- install-dev.sh
+++ install-dev.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 # Tools
-yay -S neovim gnupg pass git-credential-manager-core 
+yay -S neovim gnupg pass git-credential-manager-core
 
 # Setup GCM
 sudo gpg --gen-key | sed '3!d' | xargs pass init {}
 git config --global credential.credentialStore gpg
+git-credential-manager configure
