57cfe6f50f7efab0324dbb29aa632ae3c5e1736a
OliverGeneser <32340667+OliverGeneser@users.noreply.github.com>
2023-11-26 14:23:22 +0100

    Added openssh and libfido2

--- install-dev.sh
+++ install-dev.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 # Tools
-yay -S --answerdiff None --answerclean None neovim gnupg pass git-credential-manager-core git-credential-manager-core-extras ttf-ms-fonts
-
+yay -S --answerdiff None --answerclean None vi neovim lazygit nvm openssh libfido2 ttf-ms-fonts
+#gnupg pass git-credential-manager-core git-credential-manager-core-extras
 # Setup GCM
 # sudo gpg --gen-key | sed '3!d' | xargs pass init {}
-git config --global credential.credentialStore gpg
+#git config --global credential.credentialStore gpg
 
-echo "run gpg --gen-key"
-echo "run pass init <gpg-id>"
-echo "run git-credential-manager configure"
+#echo "run gpg --gen-key"
+#echo "run pass init <gpg-id>"
+#echo "run git-credential-manager configure"
