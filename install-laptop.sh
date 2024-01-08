56df7baad324f25e521505996afe712c6d80ba94
OliverGeneser <32340667+OliverGeneser@users.noreply.github.com>
2024-01-08 16:27:59 +0100

    Added check for yay

--- install-laptop.sh
+++ install-laptop.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+# Check if yay is installed
+ISyay=/sbin/yay
+
+if [ -f "$ISyay" ]; then
+    printf "\n%s - yay was located\n"
+else 
+    printf "\n%s - yay was NOT located\n"
+    exit
+fi
+
 # TLP
 yay -S tlp tlp-rdw
 
--- install-nvidia.sh
+++ install-nvidia.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 # https://wiki.hyprland.org/Nvidia/
 
+# Check if yay is installed
+ISyay=/sbin/yay
+
+if [ -f "$ISyay" ]; then
+    printf "\n%s - yay was located\n"
+else 
+    printf "\n%s - yay was NOT located\n"
+    exit
+fi
+
 # Tools
 yay -S hyprland-git nvidia-dkms qt5-wayland qt5ct libva libva-nvidia-driver-git linux-headers
 
