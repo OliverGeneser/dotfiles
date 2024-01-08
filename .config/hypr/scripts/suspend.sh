0be7413b96ed0d3a77d9fc488ddb0f7459a20e61
OliverGeneser <32340667+OliverGeneser@users.noreply.github.com>
2024-01-08 18:03:41 +0100

    Styled swaylock and changed wallpaper

--- .config/hypr/scripts/suspend.sh
+++ .config/hypr/scripts/suspend.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 swayidle -w timeout 120 'hyprctl dispatch dpms off' \
 	resume 'hyprctl dispatch dpms on' \
-    timeout 300 'swaylock -f -c 000000' &
+    timeout 300 'swaylock -f -c ~/.config/swaylock/config' \
+    before-sleep 'swaylock -f -c ~/.config/swaylock/config' &
