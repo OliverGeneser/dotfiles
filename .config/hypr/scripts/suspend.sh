3fb75c5093cc6d7001fa61fbf6fe394b301aaf9c
OliverGeneser <32340667+OliverGeneser@users.noreply.github.com>
2024-01-08 20:01:35 +0100

    Fixed suspend script

--- .config/hypr/scripts/suspend.sh
+++ .config/hypr/scripts/suspend.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 swayidle -w timeout 120 'hyprctl dispatch dpms off' \
 	resume 'hyprctl dispatch dpms on' \
-    timeout 300 'swaylock -f -c ~/.config/swaylock/config' \
-    before-sleep 'swaylock -f -c ~/.config/swaylock/config' &
+    timeout 300 'swaylock -f -c 000000' \
+    before-sleep 'swaylock -f -c 000000' &
