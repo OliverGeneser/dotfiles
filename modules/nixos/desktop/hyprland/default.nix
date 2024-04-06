6a9af86fd4ce447fc04ac75b6d1eb34e72daef41
OliverGeneser <32340667+OliverGeneser@users.noreply.github.com>
2024-04-06 14:42:31 +0200

    refactor: move hyprland, greetd and xdgportal

---
+++ modules/nixos/desktop/hyprland/default.nix
@@ -0,0 +1,41 @@
+{
+  options,
+  config,
+  lib,
+  pkgs,
+  inputs,
+  ...
+}:
+with lib;
+with lib.custom; let
+  cfg = config.desktop.hyprland;
+in {
+  options.desktop.hyprland = with types; {
+    enable = mkBoolOpt false "Enable or disable the hyprland window manager.";
+  };
+
+  config = mkIf cfg.enable {
+    programs.hyprland = {
+        enable = true;
+        xwayland.enable = true;
+    };
+
+
+
+    desktop.addons.greetd.enable = true;
+    desktop.addons.xdg-portal.enable = true;
+
+    environment.sessionVariables.NIXOS_OZONE_WL = "1"; # Hint electron apps to use wayland
+
+    environment.systemPackages = with pkgs; [
+      grim
+      pulseaudio
+    ];
+
+    # Hyprland configuration files
+    home.configFile = {
+      "hypr/launch".source = ./launch;
+      "hypr/hyprland.conf".source = ./hyprland.conf;
+    };
+  };
+}
