4283fd09633c4bd3c6c90080ce446f00545625ab
Oliver Geneser <hello@geneser.xyz>
2024-06-19 14:37:29 +0200

    fix: update laptop battery config

--- modules/nixos/hardware/battery/default.nix
+++ modules/nixos/hardware/battery/default.nix
@@ -19,8 +19,11 @@ in {
         CPU_SCALING_GOVERNOR_ON_AC = "performance";
         CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
 
-        CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
         CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
+        CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
+
+        PLATFORM_PROFILE_ON_AC = "performance";
+        PLATFORM_PROFILE_ON_BAT = "low-power";
 
         CPU_MIN_PERF_ON_AC = 0;
         CPU_MAX_PERF_ON_AC = 100;
@@ -30,6 +33,9 @@ in {
         #Optional helps save long term battery health
         START_CHARGE_THRESH_BAT0 = 60; # 60 and bellow it starts to charge
         STOP_CHARGE_THRESH_BAT0 = 80; # 80 and above it stops charging
+
+        RUNTIME_PM_ON_AC = "auto";
+        RUNTIME_PM_ON_BAT = "auto";
       };
     };
   };
