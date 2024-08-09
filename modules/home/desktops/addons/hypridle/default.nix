5d0af741622e2bdecf56a034f820a6ad9304c640
Oliver Geneser <hello@geneser.xyz>
2024-08-09 12:08:45 +0200

    feat: decrease brightness on idle

--- modules/home/desktops/addons/hypridle/default.nix
+++ modules/home/desktops/addons/hypridle/default.nix
@@ -1,4 +1,5 @@
 {
+  pkgs,
   config,
   lib,
   ...
@@ -26,6 +27,17 @@ in {
           {
             timeout = 300;
             on-timeout = "hyprlock";
+          }
+          {
+            timeout = 120; # 2.5min.
+            on-timeout = "${pkgs.brightnessctl}/bin/brightnessctl - s set 10"; # set monitor backlight to minimum, avoid 0 on OLED monitor.
+            on-resume = "${pkgs.brightnessctl}/bin/brightnessctl - r"; # monitor backlight restore.
+          }
+          # turn off keyboard backlight, comment out this section if you dont have a keyboard backlight.
+          {
+            timeout = 120; # 2.5min.
+            on-timeout = "${pkgs.brightnessctl}/bin/brightnessctl - sd rgb:kbd_backlight set 0"; # turn off keyboard backlight.
+            on-resume = "${pkgs.brightnessctl}/bin/brightnessctl - rd rgb:kbd_backlight"; # turn on keyboard backlight.
           }
           {
             timeout = 900;
