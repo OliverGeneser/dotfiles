44a57f726329ea7944fd50805cb145788b91c571
Oliver Geneser <hello@geneser.xyz>
2024-08-19 19:19:27 +0200

    feat: migrate neovim config back to lazy

--- modules/home/cli/editors/nvim/default.nix
+++ modules/home/cli/editors/nvim/default.nix
@@ -1,41 +1,98 @@
-{
-  pkgs,
-  lib,
-  inputs,
-  config,
-  ...
+{ pkgs
+, lib
+, inputs
+, config
+, ...
 }:
 with lib;
 with lib.custom;
 with inputs; let
   cfg = config.cli.editors.nvim;
-in {
-  imports =
-    [
-      nixvim.homeManagerModules.nixvim
-    ]
-    ++ lib.snowfall.fs.get-non-default-nix-files ./.;
 
+  treesitterWithGrammars = (pkgs.vimPlugins.nvim-treesitter.withPlugins (p: [
+    p.bash
+    p.comment
+    p.css
+    p.dockerfile
+    p.fish
+    p.gitattributes
+    p.gitignore
+    p.go
+    p.gomod
+    p.gowork
+    p.hcl
+    p.javascript
+    p.jq
+    p.json5
+    p.json
+    p.lua
+    p.make
+    p.markdown
+    p.nix
+    p.python
+    p.rust
+    p.toml
+    p.typescript
+    p.vue
+    p.yaml
+  ]));
+
+  treesitter-parsers = pkgs.symlinkJoin {
+    name = "treesitter-parsers";
+    paths = treesitterWithGrammars.dependencies;
+  };
+
+in
+{
   options.cli.editors.nvim = with types; {
     enable = mkBoolOpt false "enable neovim editor";
   };
 
   config =
     mkIf
-    cfg.enable
-    {
-      programs.neovim = {
-        viAlias = true;
-        vimAlias = true;
-        defaultEditor = true;
-      };
+      cfg.enable
+      {
+        home.packages = with pkgs; [
+          ripgrep
+          fd
+          go
+          lua-language-server
+          rustup
+          black
+        ];
 
-      programs.nixvim = {
-        enable = true;
-        extraPlugins = with pkgs.vimPlugins; [plenary-nvim];
-        defaultEditor = true;
-        viAlias = true;
-        vimAlias = true;
+        programs.neovim = {
+          enable = true;
+          #package = pkgs.neovim;
+          viAlias = true;
+          vimAlias = true;
+          defaultEditor = true;
+          coc.enable = false;
+          withNodeJs = true;
+
+          plugins = [
+            treesitterWithGrammars
+          ];
+        };
+
+        home.file = {
+          ".config/nvim" = {
+            source = ./nvim;
+            recursive = true;
+          };
+        };
+
+        home.file.".config/nvim/lua/geneser/init.lua".text = ''
+          require("geneser.set")
+          require("geneser.remap")
+          vim.opt.runtimepath:append("${treesitter-parsers}")
+        '';
+
+        # Treesitter is configured as a locally developed module in lazy.nvim
+        # we hardcode a symlink here so that we can refer to it in our lazy config
+        home.file.".local/share/nvim/nix/nvim-treesitter/" = {
+          recursive = true;
+          source = treesitterWithGrammars;
+        };
       };
-    };
 }
---
+++ modules/home/cli/editors/nvim/nvim/init.lua
@@ -0,0 +1,104 @@
+-- Bootstrap lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+  if vim.v.shell_error ~= 0 then
+    vim.api.nvim_echo({
+      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+      { out, "WarningMsg" },
+      { "\nPress any key to exit..." },
+    }, true, {})
+    vim.fn.getchar()
+    os.exit(1)
+  end
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- Make sure to setup `mapleader` and `maplocalleader` before
+-- loading lazy.nvim so that mappings are correct.
+-- This is also a good place to setup other settings (vim.opt)
+vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
+
+-- Setup lazy.nvim
+require("lazy").setup("plugins",{
+    rocks = { enabled = false },
+    dev = {
+        path = "~/.local/share/nvim/nix",
+        fallback = false,
+    }
+})
+
+require("geneser.set")
+require("geneser.remap")
+
+vim.cmd.colorscheme "catppuccin-mocha"
+
+local augroup = vim.api.nvim_create_augroup
+local geneser_group = augroup("Geneser", {})
+
+local autocmd = vim.api.nvim_create_autocmd
+local yank_group = augroup("HighlightYank", {})
+
+function R(name)
+	require("plenary.reload").reload_module(name)
+end
+
+autocmd("TextYankPost", {
+	group = yank_group,
+	pattern = "*",
+	callback = function()
+		vim.highlight.on_yank({
+			higroup = "IncSearch",
+			timeout = 40,
+		})
+	end,
+})
+
+autocmd({ "BufWritePre" }, {
+	group = geneser_group,
+	pattern = "*",
+	command = [[%s/\s\+$//e]],
+})
+
+autocmd("LspAttach", {
+	group = geneser_group,
+	callback = function(e)
+		local opts = { buffer = e.buf }
+		vim.keymap.set("n", "gd", function()
+			vim.lsp.buf.definition()
+		end, opts)
+		vim.keymap.set("n", "K", function()
+			vim.lsp.buf.hover()
+		end, opts)
+		vim.keymap.set("n", "<leader>vws", function()
+			vim.lsp.buf.workspace_symbol()
+		end, opts)
+		vim.keymap.set("n", "<leader>vd", function()
+			vim.diagnostic.open_float()
+		end, opts)
+		vim.keymap.set("n", "<leader>vca", function()
+			vim.lsp.buf.code_action()
+		end, opts)
+		vim.keymap.set("n", "<leader>vrr", function()
+			vim.lsp.buf.references()
+		end, opts)
+		vim.keymap.set("n", "<leader>vrn", function()
+			vim.lsp.buf.rename()
+		end, opts)
+		vim.keymap.set("i", "<C-h>", function()
+			vim.lsp.buf.signature_help()
+		end, opts)
+		vim.keymap.set("n", "[d", function()
+			vim.diagnostic.goto_next()
+		end, opts)
+		vim.keymap.set("n", "]d", function()
+			vim.diagnostic.goto_prev()
+		end, opts)
+	end,
+})
+
+vim.g.netrw_browse_split = 0
+vim.g.netrw_banner = 0
+vim.g.netrw_winsize = 25
