4a05986fb1633fa2b13fd70fbed9101234b90a4e
Oliver Geneser <hello@geneser.xyz>
2024-08-27 11:22:58 +0200

    fix: close lid

--- modules/home/desktops/hyprland/keybindings.nix
+++ modules/home/desktops/hyprland/keybindings.nix
@@ -9,13 +9,10 @@ with lib; let
     #!/usr/bin/env bash
 
     if grep open /proc/acpi/button/lid/LID0/state; then
-    		hyprctl keyword monitor "eDP-1, 2256x1504@60, 0x0, 1"
+    	echo "Lid open!"
     else
-    		if [[ `hyprctl monitors | grep "Monitor" | wc -l` != 1 ]]; then
-    				hyprctl keyword monitor "eDP-1, disable"
-    		else
-    				systemctl suspend
-    		fi
+        echo "Lid closed!"
+    	systemctl suspend
     fi
   '';
 
