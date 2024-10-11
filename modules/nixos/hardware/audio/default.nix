8fee0b90d2a4c6b763804c5c0ea0a3a75a293673
Oliver Geneser <hello@geneser.xyz>
2024-10-11 18:42:59 +0200

    fix: start noisetorch on boot

--- modules/nixos/hardware/audio/default.nix
+++ modules/nixos/hardware/audio/default.nix
@@ -1,13 +1,13 @@
-{
-  config,
-  pkgs,
-  lib,
-  ...
+{ config
+, pkgs
+, lib
+, ...
 }:
 with lib;
 with lib.custom; let
   cfg = config.hardware.audio;
-in {
+in
+{
   options.hardware.audio = with types; {
     enable = mkBoolOpt false "Enable or disable pipewire";
   };
@@ -26,6 +26,26 @@ in {
     };
 
     programs.noisetorch.enable = true;
+
+    systemd.user = {
+      services.noisetorchAutostart = {
+        enable = true;
+        wantedBy = [ "default.target" ];
+        description = "Auto Start Noisetorch Service";
+        requires = [ "sys-devices-pci0000:00-0000:00:14.0-usb3-3\\x2d1-3\\x2d1:1.0-sound-card2-controlC2.device" ];
+        after = [ "pipewire.service" "sys-devices-pci0000:00-0000:00:14.0-usb3-3\\x2d1-3\\xd21:1.0-sound-card2-controlC2.device" ];
+
+        serviceConfig = {
+          Type = "simple";
+          ExecStart = "${pkgs.noisetorch}/bin/noisetorch -i -s alsa_input.usb-Solid_State_System_Co._Ltd._DRELANMIC_010000000000-00.mono-fallback -t 95";
+          ExecStop = "${pkgs.noisetorch}/bin/noisetorch -u";
+          ExecStartPre="/run/current-system/sw/bin/sleep 5s";
+          RemainAfterExit = "yes";
+          Restart = "on-failure";
+          RestartSec = "3";
+        };
+      };
+    };
 
     environment.systemPackages = with pkgs; [
       pulsemixer
