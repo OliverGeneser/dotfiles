e3931ae34f0d5d7fa95f22335a849add226c6f39
Oliver Geneser <hello@geneser.xyz>
2024-10-17 13:13:08 +0200

    feat: add neovim formatting and linting

---
+++ modules/home/cli/editors/nvim/nvim/lua/geneser/lazy/formatting.lua
@@ -0,0 +1,36 @@
+return {
+	"stevearc/conform.nvim",
+
+	event = { "BufReadPre", "BufNewFile" },
+	config = function()
+		local conform = require("conform")
+
+		conform.setup({
+			log_level = vim.log.levels.DEBUG,
+			formatters_by_ft = {
+				javascript = { "prettier" },
+				typescript = { "prettier" },
+				javascriptreact = { "prettier" },
+				typescriptreact = { "prettier" },
+				svelte = { "prettier" },
+				astro = { "prettier" },
+				css = { "prettier" },
+				html = { "prettier" },
+				json = { "prettier" },
+				yaml = { "prettier" },
+				markdown = { "prettier" },
+				graphql = { "prettier" },
+				lua = { "stylua" },
+				python = { "isort", "black" },
+			},
+		})
+
+		vim.keymap.set({ "n", "v" }, "<leader>f", function()
+			conform.format({
+				lsp_fallback = true,
+				async = true,
+				--timeout_ms = 5000,
+			})
+		end, { desc = "Format file or range (in visual mode)" })
+	end,
+}
