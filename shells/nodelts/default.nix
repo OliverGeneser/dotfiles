f5bf1082a37b670db97343953962fe98e598536b
Oliver Geneser <hello@geneser.xyz>
2024-10-27 15:24:48 +0100

    feat: add lts node shell

---
+++ shells/nodelts/default.nix
@@ -0,0 +1,29 @@
+{
+  # Snowfall Lib provides a customized `lib` instance with access to your flake's library
+  # as well as the libraries available from your flake's inputs.
+  lib
+, # You also have access to your flake's inputs.
+  inputs
+, # The namespace used for your flake, defaulting to "internal" if not set.
+  namespace
+, # All other arguments come from NixPkgs. You can use `pkgs` to pull shells or helpers
+  # programmatically or you may add the named attributes as arguments here.
+  pkgs
+, mkShell
+, ...
+}:
+mkShell {
+  # Current LTS node 20 https://endoflife.date/nodejs
+  packages = with pkgs; [
+    nodejs_20
+    corepack_20
+    libuuid
+    zip
+  ];
+
+  shellHook = ''
+    export LD_LIBRARY_PATH="${pkgs.lib.makeLibraryPath [pkgs.libuuid]}"
+    node --version
+  '';
+}
+
