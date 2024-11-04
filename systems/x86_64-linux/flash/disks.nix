a9b0be467c1a3903abab4d4411cdf7d624d7ef96
Oliver Geneser <hello@geneser.xyz>
2024-11-04 16:33:25 +0100

    fix: update disk config for flash system

--- systems/x86_64-linux/flash/disks.nix
+++ systems/x86_64-linux/flash/disks.nix
@@ -3,7 +3,7 @@
     disk = {
       main = {
         type = "disk";
-        device = "/dev/disk/by-id/nvme-WD_Blue_SN580_1TB_242733801331";
+        device = "/dev/disk/by-id/ata-Samsung_SSD_840_EVO_120GB_S1D5NSAF921113R";
         content = {
           type = "gpt";
           partitions = {
@@ -64,61 +64,20 @@
           };
         };
       };
-      tank0 = {
+      big1 = {
         type = "disk";
-        device = "/dev/disk/by-id/ata-ST2000DM001-1E6164_Z1E55KEJ";
+        device = "/dev/disk/by-id/ata-HUH721212ALE601_8HJ6EGGH";
         content = {
           type = "gpt";
           partitions = {
-            zfs = {
+            root = {
               size = "100%";
               content = {
-                type = "zfs";
-                pool = "tank";
+                type = "filesystem";
+                format = "ext4";
+                mountpoint = "/";
               };
             };
-          };
-        };
-      };
-      tank1 = {
-        type = "disk";
-        device = "/dev/disk/by-id/ata-ST3160812AS_4LS1JB4F";
-        content = {
-          type = "gpt";
-          partitions = {
-            zfs = {
-              size = "100%";
-              content = {
-                type = "zfs";
-                pool = "tank";
-              };
-            };
-          };
-        };
-      };
-    };
-    zpool = {
-      tank = {
-        type = "zpool";
-        mode = "mirror";
-        options = {
-          ashift = "12";
-          autotrim = "on";
-          # Workaround: cannot import 'zroot': I/O error in disko tests
-          cachefile = "none";
-        };
-        rootFsOptions = {
-          compression = "zstd";
-          "com.sun:auto-snapshot" = "false";
-        };
-
-        postCreateHook = "zfs list -t snapshot -H -o name | grep -E '^tank@blank$' || zfs snapshot tank@blank";
-
-        datasets = {
-          vault = {
-            type = "zfs_fs";
-            mountpoint = "/vault";
-            options."com.sun:auto-snapshot" = "true";
           };
         };
       };
