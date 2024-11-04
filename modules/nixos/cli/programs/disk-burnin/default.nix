1fc8cf3da8e60a7363dedd9b93314644f06175b8
Oliver Geneser <hello@geneser.xyz>
2024-11-04 16:15:45 +0100

    feat: add burnin script and flash test system

---
+++ modules/nixos/cli/programs/disk-burnin/default.nix
@@ -0,0 +1,21 @@
+{ config
+, lib
+, pkgs
+, ...
+}:
+with lib;
+with lib.custom; let
+  cfg = config.cli.programs.disk-burnin;
+  disk-burnin = pkgs.writeShellScriptBin "disk-burnin.sh" (builtins.readFile ./disk-burnin);
+in
+{
+  options.cli.programs.disk-burnin = {
+    enable = mkEnableOption "Enable disk burnin script";
+  };
+
+  config = mkIf cfg.enable {
+    environment.systemPackages = [
+      disk-burnin
+    ];
+  };
+}
