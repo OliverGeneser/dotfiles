5f40a192996c210ce9a4db2d5b72ba65262fe4a4
Oliver Geneser <hello@geneser.xyz>
2024-11-30 12:34:57 +0100

    feat: add lo-res font

---
+++ packages/lo-res/default.nix
@@ -0,0 +1,12 @@
+{ stdenv, ... }:
+stdenv.mkDerivation {
+  pname = "lo-res";
+  version = "0.1.0";
+
+  src = ./LoRes;
+
+  installPhase = ''
+    mkdir -p $out/share/fonts
+    cp -R $src $out/share/fonts/truetype/
+  '';
+}
