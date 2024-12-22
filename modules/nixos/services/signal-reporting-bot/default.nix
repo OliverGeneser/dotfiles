a469e08b36c37b402b57daf7aa3f8cbf706e1fd2
Oliver Geneser <hello@geneser.xyz>
2024-12-22 08:31:23 +0100

    fix: use raw jq output in smart reporting

--- modules/nixos/services/signal-reporting-bot/default.nix
+++ modules/nixos/services/signal-reporting-bot/default.nix
@@ -60,9 +60,9 @@ with lib.custom; let
         SMART_ATTRIBUTES=""
 
         if [[ "$disk" == /dev/sd* ]]; then
-            SMART_ATTRIBUTES=$(echo "$RAW" | ${pkgs.jq}/bin/jq '.ata_smart_attributes.table[] | select(.id == 5 or .id == 196 or .id == 197 or .id == 198) | "\(.name): \(.raw.value)"' )
+            SMART_ATTRIBUTES=$(echo "$RAW" | ${pkgs.jq}/bin/jq -r '.ata_smart_attributes.table[] | select(.id == 5 or .id == 196 or .id == 197 or .id == 198) | "\(.name): \(.raw.value)"' )
         elif [[ "$disk" == /dev/nvme* ]]; then
-            SMART_ATTRIBUTES=$(echo "$RAW" | ${pkgs.jq}/bin/jq '.nvme_smart_health_information_log | {critical_warning, available_spare, percentage_used, media_errors, num_err_log_entries} | to_entries | .[] | "\(.key): \(.value)"' )
+            SMART_ATTRIBUTES=$(echo "$RAW" | ${pkgs.jq}/bin/jq -r '.nvme_smart_health_information_log | {critical_warning, available_spare, percentage_used, media_errors, num_err_log_entries} | to_entries | .[] | "\(.key): \(.value)"' )
         else
             SMART_ATTRIBUTES="$disk can't find SMART attributes."
         fi
