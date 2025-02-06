d212ac13964ffc6dfac36cf8db5c777a9b968dc1
Oliver Geneser <hello@geneser.xyz>
2025-02-06 10:40:35 +0100

    feat: use cachix for wezterm

--- modules/home/cli/terminals/wezterm/default.nix
+++ modules/home/cli/terminals/wezterm/default.nix
@@ -13,6 +13,11 @@ in {
   };
 
   config = mkIf cfg.enable {
+    nix.settings = {
+      substituters = ["https://wezterm.cachix.org"];
+      trusted-public-keys = ["wezterm.cachix.org-1:kAbhjYUC9qvblTE+s7S+kl5XM1zVa4skO+E/1IDWdH0="];
+    };
+
     programs.wezterm = {
       enable = true;
       package = inputs.wezterm.packages.${pkgs.system}.default;
