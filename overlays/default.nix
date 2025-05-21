5645004bbdccd196123c9828b7848ac89d49b502
Oliver Geneser <hello@geneser.xyz>
2025-05-21 09:17:56 +0200

    chore: update packages

--- overlays/default.nix
+++ overlays/default.nix
@@ -2,23 +2,23 @@
   imports = [inputs.flake-parts.flakeModules.easyOverlay];
 
   flake.overlays = rec {
-    upstreams = inputs.nixpkgs.lib.composeManyExtensions [webcord gamemode];
+    upstreams = inputs.nixpkgs.lib.composeManyExtensions [webcord];
+
+    #gamemode = _: prev: {
+    #  gamemode = prev.gamemode.overrideAttrs (oldAttrs: {
+    #    src = prev.fetchFromGitHub {
+    #      owner = "FeralInteractive";
+    #      repo = "gamemode";
+    #      tag = "1.8.2";
+    #      hash = "sha256-V0rewbSVOGFqJqXyCz4jXpuDM0EfjdkpGPl+WdDwI5I=";
+    #    };
+    #  });
+    #};
 
     webcord = _: prev: {
       webcord = prev.webcord.override {
         electron = prev.electron_32;
       };
-    };
-
-    gamemode = _: prev: {
-      gamemode = prev.gamemode.overrideAttrs (oldAttrs: {
-        src = prev.fetchFromGitHub {
-          owner = "FeralInteractive";
-          repo = "gamemode";
-          tag = "1.8.2";
-          hash = "sha256-V0rewbSVOGFqJqXyCz4jXpuDM0EfjdkpGPl+WdDwI5I=";
-        };
-      });
     };
   };
 }
--- system/nix/nixpkgs.nix
+++ system/nix/nixpkgs.nix
@@ -10,7 +10,7 @@
     config.permittedInsecurePackages = [
       "electron-25.9.0"
       "electron-32.3.3"
-      "beekeeper-studio-5.1.5"
+      "beekeeper-studio-5.2.9"
     ];
 
     overlays = [
