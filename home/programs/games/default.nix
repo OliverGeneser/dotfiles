119918a34d92746bf94326f9abad6286b5c0d554
Oliver Geneser <hello@geneser.xyz>
2025-06-16 21:50:56 +0200

    feat: enable lutris

--- home/programs/games/default.nix
+++ home/programs/games/default.nix
@@ -10,7 +10,7 @@
     (inputs.nix-gaming.packages.${pkgs.system}.osu-lazer-bin.override {pipewire_latency = "128/48000";})
     gamescope
     prismlauncher
-    ## (lutris.override {extraPkgs = p: [p.libnghttp2];})
+    (lutris.override {extraPkgs = p: [p.libnghttp2];})
     winetricks
     protontricks
   ];
