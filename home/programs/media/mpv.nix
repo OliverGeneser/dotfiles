485e9d0142ad3b3c1e0d217a237c64f8c0a6c5fe
Oliver Geneser <hello@geneser.xyz>
2025-08-09 12:23:57 +0200

    feat(mpv): configure wrapper package, scripts, and settings
    - Replace top-level defaultProfiles and scripts with a wrapper package for mpv-unwrapped, including scripts: uosc, sponsorblock, mpris.
    - Enable Wayland support via mpv-unwrapped.override { waylandSupport = true }.
    - Add configuration: profile = "high-quality", ytdl-format = "bestvideo+bestaudio", cache-default = 4000000.

--- home/programs/media/mpv.nix
+++ home/programs/media/mpv.nix
@@ -1,7 +1,25 @@
 {pkgs, ...}: {
   programs.mpv = {
     enable = true;
-    defaultProfiles = ["gpu-hq"];
-    scripts = [pkgs.mpvScripts.mpris];
+
+    package = (
+      pkgs.mpv-unwrapped.wrapper {
+        scripts = with pkgs.mpvScripts; [
+          uosc
+          sponsorblock
+          mpris
+        ];
+
+        mpv = pkgs.mpv-unwrapped.override {
+          waylandSupport = true;
+        };
+      }
+    );
+
+    config = {
+      profile = "high-quality";
+      ytdl-format = "bestvideo+bestaudio";
+      cache-default = 4000000;
+    };
   };
 }
