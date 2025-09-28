9e2709c55adcb0a730d4c97d3181d0d860887228
Oliver Geneser <hello@geneser.xyz>
2025-09-28 14:32:43 +0200

    fix: resume device on all systems

--- hosts/enterprise/disks.nix
+++ hosts/enterprise/disks.nix
@@ -35,7 +35,10 @@
                 ];
                 # https://0pointer.net/blog/unlocking-luks2-volumes-with-tpm2-fido2-pkcs11-security-hardware-on-systemd-248.html
                 settings = {
-                  crypttabExtraOpts = ["fido2-device=auto" "token-timeout=10"];
+                  crypttabExtraOpts = [
+                    "fido2-device=auto"
+                    "token-timeout=10"
+                  ];
                 };
                 content = {
                   type = "btrfs";
