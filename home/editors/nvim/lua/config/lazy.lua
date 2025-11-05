19404190068e3b258548675ff515c7bb674794bb
Oliver Geneser <hello@geneser.xyz>
2025-11-05 13:37:29 +0100

    feat: add prettier to lazyvim

--- home/editors/nvim/lua/config/lazy.lua
+++ home/editors/nvim/lua/config/lazy.lua
@@ -29,6 +29,7 @@ require("lazy").setup({
 		{ import = "lazyvim.plugins.extras.lang.tailwind" },
 		{ import = "lazyvim.plugins.extras.lang.typescript" },
 		{ import = "lazyvim.plugins.extras.formatting.biome" },
+		{ import = "lazyvim.plugins.extras.formatting.prettier" },
 		{ import = "lazyvim.plugins.extras.editor.harpoon2" },
 
 		-- import/override with your plugins
