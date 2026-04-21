8fbed8929938a5043dfcc9f802ca22be22d3bbe9
Oliver Geneser <hello@geneser.xyz>
2026-04-21 21:10:21 +0200

    feat: update packages

--- system/programs/hyprland/settings.nix
+++ system/programs/hyprland/settings.nix
@@ -66,7 +66,6 @@ in {
         shadow = {
           enabled = true;
           color = "rgba(00000055)";
-          ignore_window = true;
           offset = "0 15";
           range = 100;
           render_power = 2;
