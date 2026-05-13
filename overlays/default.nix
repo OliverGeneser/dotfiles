be4b5f267babbb457fe13197748611bffb512488
Oliver Geneser <hello@geneser.xyz>
2026-05-13 10:36:27 +0200

    feat: update packages

--- overlays/default.nix
+++ overlays/default.nix
@@ -21,11 +21,11 @@
 
     bun = self: super: {
       bun = super.bun.overrideAttrs (final: prev: {
-        version = "1.3.13";
+        version = "1.3.14";
 
         src = super.fetchurl {
           url = "https://github.com/oven-sh/bun/releases/download/bun-v${final.version}/bun-linux-x64.zip";
-          hash = "sha256-ecB3H6i5LDOq5B4VoODTB+qZ0OLwAxfHHGxTI3p44lo=";
+          hash = "sha256-lR7iruhV8IWVruxiJSJqKY0/6oOj3NZGXAnLzN9+hI8=";
         };
       });
     };
@@ -68,13 +68,13 @@
 
     turso-cli = self: super: {
       turso-cli = super.turso-cli.overrideAttrs (final: prev: {
-        version = "1.0.23";
+        version = "1.0.24";
 
         vendorHash = "sha256-Cb4/KA9jfI/pNHbJqLWtm9oEXfMHGBS46J9o3lL4/Tk=";
 
         src = prev.src.override {
           rev = "v${final.version}";
-          hash = "sha256-35ggyhyBQ/N9yNZvt8lwL/LmkOK0ZYjWlIQl+pmfx98=";
+          hash = "sha256-3fKEFK4zCeKEYfiBJ7so5pZ3ZQC2td80XKWN3GKFWLA=";
         };
       });
     };
