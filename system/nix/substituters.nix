045179352cca9bd0258e80154cdb09ba09ea3177
Oliver Geneser <hello@geneser.xyz>
2026-06-01 09:30:23 +0200

    feat: add vicinae cache

--- system/nix/substituters.nix
+++ system/nix/substituters.nix
@@ -10,6 +10,7 @@
       "https://hyprland.cachix.org"
       "https://nix-community.cachix.org"
       "https://nix-gaming.cachix.org"
+      "https://vicinae.cachix.org"
       "https://yazi.cachix.org"
       "https://prismlauncher.cachix.org"
     ];
@@ -23,6 +24,7 @@
       "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       "nix-gaming.cachix.org-1:nbjlureqMbRAxR1gJ/f3hxemL9svXaZF/Ees8vCUUs4="
+      "vicinae.cachix.org-1:1kDrfienkGHPYbkpNj1mWTr7Fm1+zcenzgTizIcI3oc="
       "yazi.cachix.org-1:Dcdz63NZKfvUCbDGngQDAZq6kOroIrFoyO064uvLh8k="
       "prismlauncher.cachix.org-1:9/n/FGyABA2jLUVfY+DEp4hKds/rwO+SCOtbOkDzd+c="
     ];
