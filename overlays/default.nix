23e6a33eae63858bfbe4f6b0b8d2bd3943eae72f
Oliver Geneser <hello@geneser.xyz>
2026-06-23 19:27:55 +0200

    feat: update packages

--- overlays/default.nix
+++ overlays/default.nix
@@ -72,13 +72,13 @@
     turso-cli = self: super: {
       turso-cli = super.turso-cli.overrideAttrs (
         final: prev: {
-          version = "1.0.28";
+          version = "1.0.29";
 
           vendorHash = "sha256-4OIJVL3N2mWOw7ZDP4xFCxa9zmUTPCA8N79TVoi1lys=";
 
           src = prev.src.override {
             rev = "v${final.version}";
-            hash = "sha256-5mtyMiKAL30NLPCL5mJw9XSUw81GPEKDy+wtO5Hdk5M=";
+            hash = "sha256-jsvXglCf/FyJ3tasnOywXLA20k94yzbojPdX+dZVPfw=";
           };
         }
       );
